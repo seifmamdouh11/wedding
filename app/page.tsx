eb84a13d33d46aa2a2da739a575b13459c850bc7
Seif Mattar <seifmamdouh@yahoo.com>
2026-06-05 07:18:29 +0300

    feat: hero polish, fixed animation timing, glass info card, floating RSVP WhatsApp button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Preloader from "./components/Preloader";
 import Hero from "./components/Hero";
 import MusicToggle from "./components/MusicToggle";
+import RSVPFloat from "./components/RSVPFloat";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <Hero />
       </main>
       <MusicToggle />
+      <RSVPFloat />
     </>
   );
 }
